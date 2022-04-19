345af9f4af4ba278ca9dc9e206e290ad87482063
Kooreis <kooreis.official@gmail.com>
2022-04-19 18:00:00 -0500

    This update includes the definition of the function 'print_board'. This function prints the Sudoku board in a readable format.

--- Sudoku_Puzzle_Generator_and_Solver.py
+++ Sudoku_Puzzle_Generator_and_Solver.py
@@ -1,15 +1,4 @@
-import random
-
-def generate_board():
-    base  = 3
-    side  = base*base
-    nums  = sample(range(1, side + 1), side)
-    board = [[nums[(base*(r%base)+r//base+c)%side] for c in range(side) ] for r in range(side)]
-    rows  = [ r for g in sample(range(base),base) for r in sample(range(g*base,(g+1)*base),base) ] 
-    cols  = [ c for g in sample(range(base),base) for c in sample(range(g*base,(g+1)*base),base) ]            
-    board = [[board[r][c] for c in cols] for r in rows]
-    squares = side*side
-    empties = squares * 3//4
-    for p in map(lambda _: (randrange(side), randrange(side)), range(empties)):
-        board[p[0]][p[1]] = 0
-    return board
+def print_board(board):
+    num_len = len(str(max(map(max, board))))
+    for line in board:
+        print("[" + "][".join(f"{n or '.':{num_len}}" for n in line) + "]")
