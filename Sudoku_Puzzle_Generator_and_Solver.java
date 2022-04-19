adc96c2be855ffda3d6f23cb20b224147dd0f691
Kooreis <kooreis.official@gmail.com>
2022-04-19 18:00:00 -0500

    This part of the script defines helper methods to check if a number is already present in the current row, column, or 3x3 box.

--- Sudoku_Puzzle_Generator_and_Solver.java
+++ Sudoku_Puzzle_Generator_and_Solver.java
@@ -1,17 +1,25 @@
 ```java
-import java.util.*;
+private boolean isInRow(int row, int number) {
+        for (int i = 0; i < SIZE; i++)
+            if (board[row][i] == number)
+                return true;
+        return false;
+    }
 
-public class Sudoku {
-    int[][] board;
-    public static final int EMPTY = 0;
-    public static final int SIZE = 9;
+    private boolean isInCol(int col, int number) {
+        for (int i = 0; i < SIZE; i++)
+            if (board[i][col] == number)
+                return true;
+        return false;
+    }
 
-    public Sudoku(int[][] board) {
-        this.board = new int[SIZE][SIZE];
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                this.board[i][j] = board[i][j];
-            }
-        }
+    private boolean isInBox(int row, int col, int number) {
+        int r = row - row % 3;
+        int c = col - col % 3;
+        for (int i = r; i < r + 3; i++)
+            for (int j = c; j < c + 3; j++)
+                if (board[i][j] == number)
+                    return true;
+        return false;
     }
 ```
